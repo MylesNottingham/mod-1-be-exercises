b6eb6d9aa2839d04a5def29ff8dc20b521e621d1
Myles Nottingham <MylesNottingham@gmail.com>
2023-03-20 18:57:09 -0600

    Complete PIC - Week 1

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,2 +1,32 @@
 class Venue
+  attr_reader :name,
+              :capacity,
+              :patrons
+  
+  def initialize(name, capacity)
+      @name = name
+      @capacity = capacity
+      @patrons = []
+  end
+
+  def add_patron(patron)
+    @patrons << patron
+  end
+
+  def yell_at_patrons
+    yell = patrons.map do |patron|
+      patron.upcase
+    end
+    yell
+  end
+
+  def over_capacity?
+    @patrons.count > @capacity
+  end
+
+  def kick_out
+    while @patrons.count > @capacity
+      @patrons.shift
+    end
+  end
 end
