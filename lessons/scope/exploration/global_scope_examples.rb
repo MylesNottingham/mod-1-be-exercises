9e78c42bba2a7d2ae78a8021c7127251d7439f64
Myles Nottingham <MylesNottingham@gmail.com>
2023-03-23 13:04:42 -0600

    Complete Scope Lesson

---
+++ lessons/scope/exploration/global_scope_examples.rb
@@ -0,0 +1,79 @@
+# #### Example 1
+
+# x = 10
+# puts x
+# puts y
+
+# #### Example 2
+
+# x = 10
+# puts x
+# puts y
+# y = 20
+
+# #### Example 3
+
+# x = 10
+# def say_hello
+#   puts "Hello World!"
+# end
+# puts x
+
+# #### Example 4
+
+# def print_variable
+#   x = 4
+#   puts x
+# end
+
+# x = 2
+# print_variable
+
+# #### Example 5
+
+# def print_variable
+#   x = 4
+# end
+
+# x = 2
+# print_variable
+# puts x
+
+# #### Example 6
+
+# def print_variable
+#   x = 10
+#   puts x
+# end
+
+# print_variable
+# puts x
+
+# #### Example 7
+
+# def print_variable
+#   x = 4
+#   puts x
+# end
+
+# print_variable
+
+# #### Example 8
+
+# def print_variable
+#   x = 4
+#   puts x
+# end
+
+# x = 2
+# print_variable
+
+# #### Example 9
+
+# def print_variable
+#   x = 4
+# end
+
+# x = 2
+# print_variable
+# puts x
