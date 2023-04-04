0d99e2104f7dd5531e287b80902a22b109ae4f66
Myles Nottingham <MylesNottingham@gmail.com>
2023-04-04 11:02:11 -0600

    Complete nested_collections

---
+++ lessons/nested_collections/challenges/test.rb
@@ -0,0 +1,101 @@
+denver_biscuit_co = {
+  id: "gagUrh3806qc5hZ14F0Odw",
+  alias: "denver-biscuit-denver-11",
+  name: "Denver Biscuit",
+  image_url: "https://s3-media1.fl.yelpcdn.com/bphoto/bxPN9shgJEtwvT3Hrf_pCg/o.jpg",
+  is_claimed: true,
+  is_closed: false,
+  url: "https://www.yelp.com/biz/denver-biscuit-denver-11?adjust_creative=dDfOOhvKrbo6zoRsFCf00A&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_lookup&utm_source=dDfOOhvKrbo6zoRsFCf00A",
+  phone: "+13033777900",
+  display_phone: "(303) 377-7900",
+  review_count: 4063,
+  categories: [
+      {
+          alias: "sandwiches",
+          title: "Sandwiches"
+      },
+      {
+          alias: "breakfast_brunch",
+          title: "Breakfast & Brunch"
+      }
+  ],
+  rating: 4.5,
+  location: {
+      address1: "3237 E Colfax Ave",
+      address2: "",
+      address3: nil,
+      city: "Denver",
+      zip_code: "80206",
+      country: "US",
+      state: "CO",
+      display_address: [
+          "3237 E Colfax Ave",
+          "Denver, CO 80206"
+      ],
+      cross_streets: ""
+  },
+  coordinates: {
+      latitude: 39.740384,
+      longitude: -104.949098
+  },
+  photos: [
+      "https://s3-media1.fl.yelpcdn.com/bphoto/bxPN9shgJEtwvT3Hrf_pCg/o.jpg",
+      "https://s3-media1.fl.yelpcdn.com/bphoto/WgUIK8M236LzLjDQ5Idu6w/o.jpg",
+      "https://s3-media2.fl.yelpcdn.com/bphoto/sFG5Wuj_g-Msszto9dndmA/o.jpg"
+  ],
+  price: "$$",
+  hours: [
+      {
+          open: [
+              {
+                  is_overnight: false,
+                  start: "0800",
+                  end: "1400",
+                  day: 0
+              },
+              {
+                  is_overnight: false,
+                  start: "0800",
+                  end: "1400",
+                  day: 1
+              },
+              {
+                  is_overnight: false,
+                  start: "0800",
+                  end: "1400",
+                  day: 2
+              },
+              {
+                  is_overnight: false,
+                  start: "0800",
+                  end: "1400",
+                  day: 3
+              },
+              {
+                  is_overnight: false,
+                  start: "0800",
+                  end: "1400",
+                  day: 4
+              },
+              {
+                  is_overnight: false,
+                  start: "0800",
+                  end: "1500",
+                  day: 5
+              },
+              {
+                  is_overnight: false,
+                  start: "0800",
+                  end: "1500",
+                  day: 6
+              }
+          ],
+          hours_type: "REGULAR",
+          is_open_now: true
+      }
+  ],
+  transactions: [
+      "pickup",
+      "delivery"
+  ]
+}
