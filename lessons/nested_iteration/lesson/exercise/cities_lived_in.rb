0d9ea8bf222ba7658005ce25b3af719664f34a74
Myles Nottingham <MylesNottingham@gmail.com>
2023-04-15 16:03:27 -0600

    Complete exercise.

--- lessons/nested_iteration/lesson/exercise/cities_lived_in.rb
+++ lessons/nested_iteration/lesson/exercise/cities_lived_in.rb
@@ -6,26 +6,32 @@ cities_lived_in = {
 }
 
 
-# Problem #1: 
-# Get a unique list of all of the cities that these humans have lived in  
+# Problem #1:
+# Get a unique list of all of the cities that these humans have lived in
 # ["Philadelphia", "Fort Collins", "Seattle", "Denver", "Santa Fe", "Portland", "Lansing", "Columbus", "Austin"]
 
+unique_cities = cities_lived_in.values.flatten.uniq
 
+print unique_cities
+puts
 
-
-# Problem #2: 
-# Write code that iterates through the `cities_lived_in` hash, and returns a list of  
+# Problem #2:
+# Write code that iterates through the `cities_lived_in` hash, and returns a list of
 # names of the humans who've lived in Philadelphia.
 
-# [:michaela, :mike, :salvador]
+# [:michaela, :mike, :alex]
 # or
-# ["Michaela", "Mike", "Salvador"]
+# ["Michaela", "Mike", "Alex"]
 
+philly_residents = cities_lived_in.filter_map do |human|
+    human[0] if human[1].include?("Philadelphia")
+end
 
+print philly_residents
+puts
 
-
-# Problem #3: 
-# Create a hash that has the city as a key, and the number of people that live in it as it's value: 
+# Problem #3:
+# Create a hash that has the city as a key, and the number of people that live in it as it's value:
 
 # {
 #     "Philadelphia" => 3,
@@ -38,3 +44,11 @@ cities_lived_in = {
 #     "Columbus => 1,
 #     "Austin" => 1
 # }
+
+
+city_pop = cities_lived_in.values.flatten.each_with_object(Hash.new(0)) do |city, pop|
+    pop[city] += 1
+end
+
+print city_pop
+puts
