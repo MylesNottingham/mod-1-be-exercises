abac1b37f2d6aad7eda010346f6e67bc0646c906
Myles Nottingham <MylesNottingham@gmail.com>
2023-04-15 14:49:01 -0600

    Test: Add tests for Airconditionable.

--- lessons/modules/vehicles/spec/jeep_spec.rb
+++ lessons/modules/vehicles/spec/jeep_spec.rb
@@ -20,4 +20,12 @@ RSpec.describe Jeep do
   it 'can drive' do
     expect(@jeep.drive).to eq("All wheels go!")
   end
+
+  it 'can turn on the AC' do
+    expect(@jeep.ac_on).to eq("Chilly air coming your way!")
+  end
+
+  it 'can turn on the AC' do
+    expect(@jeep.ac_off).to eq("Temp is fine in here.")
+  end
 end
