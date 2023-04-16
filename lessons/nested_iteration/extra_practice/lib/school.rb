784bdb0080d3c7d67fb936fdc88c8f7d0d7c2b1f
Myles Nottingham <MylesNottingham@gmail.com>
2023-04-16 11:56:33 -0600

    Finish School.

--- lessons/nested_iteration/extra_practice/lib/school.rb
+++ lessons/nested_iteration/extra_practice/lib/school.rb
@@ -14,4 +14,31 @@ class School
   def assign_to_group(student)
     student.group = @groups.sample
   end
+
+  def group_all_students
+    @courses.each do |course|
+      course.students.each do |student|
+        assign_to_group(student) if student.group.nil?
+      end
+    end
+  end
+
+  def names
+    @courses.flat_map do |course|
+      course.students.map do |student|
+        student.name
+      end
+    end.uniq
+  end
+
+  def students_by_group
+    @groups.each_with_object({}) do |group, hash|
+      hash[group] = []
+      @courses.each do |course|
+        course.students.each do |student|
+            hash[group] |= [student] if student.group == group
+        end
+      end
+    end
+  end
 end
